30bac30f864eab17524d2ad337a0575e09a0144d
Pratik Kakade <Pratikkakade4618@gmail.com>
2023-10-19 18:45:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Level of Nodes/level-of-nodes.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for(int i = 0; i < V+1; i++)
+                list.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++)
+            {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+                list.get(v).add(u);
+            }
+            int X = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+            System.out.println(ob.nodeLevel(V,list,X));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution
+{
+    //Function to find the level of node X.
+    int nodeLevel(int V, ArrayList<ArrayList<Integer>> adj, int X)
+    {
+        // code here
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[V];
+        q.add(0);
+        vis[0] = true;
+    
+        int level = 0;
+
+        while (!q.isEmpty()) {
+          int n = q.size();
+          for (int i = 0; i < n; i++) {
+            int a = q.remove();
+            if (a == X)
+                return level;
+
+            for (int j = 0; j < adj.get(a).size(); j++) {
+                int arb = adj.get(a).get(j);
+                if (!vis[arb]) {
+                    vis[arb] = true;
+                    q.add(arb);
+                }
+            }
+          }
+         level++;
+        }
+        return -1;
+    }
+}
