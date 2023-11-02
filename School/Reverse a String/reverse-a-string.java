ef3dd64653cfeceb2415d88ee1b7dab0747cf64f
Pratik Kakade <Pratikkakade4618@gmail.com>
2023-11-02 20:30:51 +0530

    Added Solution - GfG to GitHub

---
+++ School/Reverse a String/reverse-a-string.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial template for Java
+
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.revStr(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static String revStr(String S) {
+        // code here
+        String ch = "";
+        for(int i = S.length()-1 ; i >= 0  ; i--)
+        {
+            ch = ch + S.charAt(i);
+        }
+        
+        return ch;
+    }
+}
