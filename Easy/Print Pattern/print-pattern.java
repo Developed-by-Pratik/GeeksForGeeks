44c1b1cc05a3ab2c7a093e9413afe8b0680bbcc7
Pratik Kakade <Pratikkakade4618@gmail.com>
2023-11-26 09:57:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Print Pattern/print-pattern.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine().trim());
+            
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.pattern(N);
+            for(int i = 0;i < ans.size();i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public List<Integer> pattern(int N){
+        // code here
+        List<Integer> v = new ArrayList<>();
+        int temp = N;
+        
+        while(temp > 0) {
+            v.add(temp);
+            temp -= 5;
+        }
+        
+        v.add(temp);
+        temp += 5;
+        
+        while(temp < N + 1) {
+            v.add(temp);
+            temp += 5;
+        }
+        
+        return v;
+    }
+}
